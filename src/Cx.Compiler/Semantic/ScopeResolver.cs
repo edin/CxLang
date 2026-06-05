b8f565f1aab78670a34c0bc9264f411f3ec2c47c
edin.omeragic <edin.omeragic@gmail.com>
2026-06-05 17:54:01 +0200

    Scope resolver

--- src/Cx.Compiler/Semantic/ScopeResolver.cs
+++ src/Cx.Compiler/Semantic/ScopeResolver.cs
@@ -36,52 +36,52 @@ internal sealed class ScopeResolver(DiagnosticBag diagnostics, SemanticModel mod
     {
         foreach (var typeAlias in program.TypeAliases)
         {
-            DeclareTopLevel(typeAlias.Name, SymbolKind.Type, typeAlias.TargetType, typeAlias.Location);
+            DeclareTopLevel(typeAlias.Name, SymbolKind.Type, typeAlias.TargetType, typeAlias.Location, typeAlias);
         }
 
         foreach (var requirement in program.Requirements)
         {
-            DeclareTopLevel(requirement.Name, SymbolKind.Type, null, requirement.Location);
+            DeclareTopLevel(requirement.Name, SymbolKind.Type, null, requirement.Location, requirement);
         }
 
         foreach (var enumNode in program.Enums)
         {
-            DeclareTopLevel(enumNode.Name, SymbolKind.Type, enumNode.Name, enumNode.Location);
+            DeclareTopLevel(enumNode.Name, SymbolKind.Type, enumNode.Name, enumNode.Location, enumNode);
         }
 
         foreach (var interfaceNode in program.Interfaces)
         {
-            DeclareTopLevel(interfaceNode.Name, SymbolKind.Type, interfaceNode.Name, interfaceNode.Location);
+            DeclareTopLevel(interfaceNode.Name, SymbolKind.Type, interfaceNode.Name, interfaceNode.Location, interfaceNode);
         }
 
         foreach (var structNode in program.Structs)
         {
-            DeclareTopLevel(structNode.Name, SymbolKind.Type, structNode.Name, structNode.Location);
+            DeclareTopLevel(structNode.Name, SymbolKind.Type, structNode.Name, structNode.Location, structNode);
         }
 
         foreach (var adapter in program.TypeAdapters)
         {
-            DeclareTopLevel(adapter.Name, SymbolKind.Type, adapter.Name, adapter.Location);
+            DeclareTopLevel(adapter.Name, SymbolKind.Type, adapter.Name, adapter.Location, adapter);
         }
 
         foreach (var union in program.TaggedUnions)
         {
-            DeclareTopLevel(union.Name, SymbolKind.Type, union.Name, union.Location);
+            DeclareTopLevel(union.Name, SymbolKind.Type, union.Name, union.Location, union);
         }
 
         foreach (var global in program.GlobalVariables)
         {
-            DeclareTopLevel(global.Name, SymbolKind.Global, global.Type, global.Location);
+            DeclareTopLevel(global.Name, SymbolKind.Global, global.Type, global.Location, global);
         }
 
         foreach (var externFunction in program.ExternFunctions)
         {
-            DeclareTopLevel(externFunction.Name, SymbolKind.Function, externFunction.ReturnType, externFunction.Location);
+            DeclareTopLevel(externFunction.Name, SymbolKind.Function, externFunction.ReturnType, externFunction.Location, externFunction);
         }
 
         foreach (var function in program.Functions.Where(function => function.OwnerType is null))
         {
-            DeclareTopLevel(function.Name, SymbolKind.Function, function.ReturnType, function.Location);
+            DeclareTopLevel(function.Name, SymbolKind.Function, function.ReturnType, function.Location, function);
         }
     }
 
@@ -97,7 +97,7 @@ internal sealed class ScopeResolver(DiagnosticBag diagnostics, SemanticModel mod
 
         foreach (var parameter in function.Parameters.Where(parameter => !parameter.IsVariadic))
         {
-            Declare(functionScope, parameter.Name, SymbolKind.Parameter, parameter.Type, parameter.Location);
+            Declare(functionScope, parameter.Name, SymbolKind.Parameter, parameter.Type, parameter.Location, parameter);
         }
 
         ResolveStatements(function.Body, functionScope);
@@ -116,10 +116,20 @@ internal sealed class ScopeResolver(DiagnosticBag diagnostics, SemanticModel mod
         switch (statement)
         {
             case LetStatement let:
-                Declare(scope, let.Name, SymbolKind.Local, let.Type, let.Location);
+                ResolveExpression(let.Initializer, scope);
+                Declare(scope, let.Name, SymbolKind.Local, let.Type, let.Location, let);
+                break;
+
+            case ReturnStatement ret:
+                ResolveExpression(ret.Expression, scope);
+                break;
+
+            case CStatement c:
+                ResolveExpression(c.Expression, scope);
                 break;
 
             case IfStatement ifStatement:
+                ResolveExpression(ifStatement.Condition, scope);
                 ResolveStatements(ifStatement.ThenBody, scope.CreateChild());
                 if (ifStatement.ElseBranch is not null)
                 {
@@ -133,16 +143,20 @@ internal sealed class ScopeResolver(DiagnosticBag diagnostics, SemanticModel mod
                 break;
 
             case WhileStatement whileStatement:
+                ResolveExpression(whileStatement.Condition, scope);
                 ResolveStatements(whileStatement.Body, scope.CreateChild());
                 break;
 
             case ForStatement forStatement:
                 var forScope = scope.CreateChild();
                 ResolveForInitializer(forStatement.Initializer, forScope);
+                ResolveExpression(forStatement.Condition, forScope);
+                ResolveExpression(forStatement.Increment, forScope);
                 ResolveStatements(forStatement.Body, forScope.CreateChild());
                 break;
 
             case ForeachStatement foreachStatement:
+                ResolveExpression(foreachStatement.IterableExpression, scope);
                 var foreachScope = scope.CreateChild();
                 DeclareForeachBinding(foreachScope, foreachStatement.IndexBinding);
                 DeclareForeachBinding(foreachScope, foreachStatement.KeyBinding);
@@ -151,8 +165,10 @@ internal sealed class ScopeResolver(DiagnosticBag diagnostics, SemanticModel mod
                 break;
 
             case SwitchStatement switchStatement:
+                ResolveExpression(switchStatement.Expression, scope);
                 foreach (var switchCase in switchStatement.Cases)
                 {
+                    ResolveExpression(switchCase.Pattern, scope);
                     ResolveStatements(switchCase.Body, scope.CreateChild());
                 }
 
@@ -160,12 +176,13 @@ internal sealed class ScopeResolver(DiagnosticBag diagnostics, SemanticModel mod
                 break;
 
             case MatchStatement matchStatement:
+                ResolveExpression(matchStatement.Expression, scope);
                 foreach (var arm in matchStatement.Arms)
                 {
                     var armScope = scope.CreateChild();
                     if (arm.BindingName is not null)
                     {
-                        Declare(armScope, arm.BindingName, SymbolKind.MatchBinding, null, arm.Location);
+                        Declare(armScope, arm.BindingName, SymbolKind.MatchBinding, null, arm.Location, arm);
                     }
 
                     ResolveStatements(arm.Body, armScope);
@@ -177,9 +194,15 @@ internal sealed class ScopeResolver(DiagnosticBag diagnostics, SemanticModel mod
 
     private void ResolveForInitializer(ForInitializerNode initializer, Scope scope)
     {
-        if (initializer is ForDeclarationInitializerNode declaration)
+        switch (initializer)
         {
-            Declare(scope, declaration.Name, SymbolKind.Local, declaration.Type, declaration.Location);
+            case ForDeclarationInitializerNode declaration:
+                ResolveExpression(declaration.Initializer, scope);
+                Declare(scope, declaration.Name, SymbolKind.Local, declaration.Type, declaration.Location, declaration);
+                break;
+            case ForExpressionInitializerNode expression:
+                ResolveExpression(expression.Expression, scope);
+                break;
         }
     }
 
@@ -190,22 +213,130 @@ internal sealed class ScopeResolver(DiagnosticBag diagnostics, SemanticModel mod
             return;
         }
 
-        Declare(scope, binding.Name, SymbolKind.ForeachBinding, binding.Type, binding.Location);
+        Declare(scope, binding.Name, SymbolKind.ForeachBinding, binding.Type, binding.Location, binding);
     }
 
-    private void Declare(Scope scope, string name, SymbolKind kind, string? type, Location location)
+    private void ResolveExpression(ExpressionNode? expression, Scope scope)
     {
-        if (scope.TryDeclare(new Symbol(name, kind, type, location)))
+        switch (expression)
         {
-            return;
+            case null:
+                return;
+            case NameExpressionNode name:
+                if (scope.TryResolve(name.SourceText, out var symbol))
+                {
+                    name.Semantic.Symbol = symbol;
+                }
+
+                break;
+            case ParenthesizedExpressionNode parenthesized:
+                ResolveExpression(parenthesized.Expression, scope);
+                break;
+            case CastExpressionNode cast:
+                ResolveExpression(cast.Expression, scope);
+                break;
+            case UnaryExpressionNode unary:
+                ResolveExpression(unary.Operand, scope);
+                break;
+            case PostfixExpressionNode postfix:
+                ResolveExpression(postfix.Operand, scope);
+                break;
+            case SizeOfExpressionNode sizeOf:
+                ResolveExpression(sizeOf.ExpressionOperand, scope);
+                break;
+            case BinaryExpressionNode binary:
+                ResolveExpression(binary.Left, scope);
+                ResolveExpression(binary.Right, scope);
+                break;
+            case ConditionalExpressionNode conditional:
+                ResolveExpression(conditional.Condition, scope);
+                ResolveExpression(conditional.WhenTrue, scope);
+                ResolveExpression(conditional.WhenFalse, scope);
+                break;
+            case ScalarRangeExpressionNode range:
+                ResolveExpression(range.Start, scope);
+                ResolveExpression(range.End, scope);
+                break;
+            case InitializerExpressionNode initializer:
+                foreach (var field in initializer.Fields)
+                {
+                    ResolveExpression(field.Value, scope);
+                }
+
+                foreach (var value in initializer.Values)
+                {
+                    ResolveExpression(value, scope);
+                }
+
+                break;
+            case FunctionExpressionNode function:
+                var functionScope = scope.CreateChild();
+                foreach (var parameter in function.Parameters.Where(parameter => !parameter.IsVariadic))
+                {
+                    Declare(functionScope, parameter.Name, SymbolKind.Parameter, parameter.Type, parameter.Location, parameter);
+                }
+
+                ResolveExpression(function.ExpressionBody, functionScope);
+                if (function.BlockBody is not null)
+                {
+                    ResolveStatements(function.BlockBody, functionScope);
+                }
+
+                break;
+            case AssignmentExpressionNode assignment:
+                ResolveExpression(assignment.Target, scope);
+                ResolveExpression(assignment.Value, scope);
+                break;
+            case CallExpressionNode call:
+                ResolveExpression(call.Callee, scope);
+                foreach (var argument in call.Arguments)
+                {
+                    ResolveExpression(argument, scope);
+                }
+
+                break;
+            case GenericCallExpressionNode call:
+                ResolveExpression(call.Callee, scope);
+                foreach (var argument in call.Arguments)
+                {
+                    ResolveExpression(argument, scope);
+                }
+
+                break;
+            case MemberExpressionNode member:
+                ResolveExpression(member.Target, scope);
+                break;
+            case IndexExpressionNode index:
+                ResolveExpression(index.Target, scope);
+                ResolveExpression(index.Index, scope);
+                break;
+        }
+    }
+
+    private Symbol? Declare(Scope scope, string name, SymbolKind kind, string? type, Location location, SyntaxNode? node = null)
+    {
+        var symbol = new Symbol(name, kind, type, location);
+        if (scope.TryDeclare(symbol))
+        {
+            if (node is not null)
+            {
+                node.Semantic.Symbol = symbol;
+            }
+
+            return symbol;
         }
 
         diagnostics.Report(location, $"Duplicate {Describe(kind)} '{name}' in the same scope.");
+        return null;
     }
 
-    private void DeclareTopLevel(string name, SymbolKind kind, string? type, Location location)
+    private void DeclareTopLevel(string name, SymbolKind kind, string? type, Location location, SyntaxNode node)
     {
-        model.RootScope.TryDeclare(new Symbol(name, kind, type, location));
+        var symbol = new Symbol(name, kind, type, location);
+        if (model.RootScope.TryDeclare(symbol))
+        {
+            node.Semantic.Symbol = symbol;
+        }
     }
 
     private static string Describe(SymbolKind kind) =>
--- tests/Cx.Compiler.Tests/ScopeResolverTests.cs
+++ tests/Cx.Compiler.Tests/ScopeResolverTests.cs
@@ -1,4 +1,8 @@
+using Cx.Compiler.Diagnostics;
+using Cx.Compiler.Semantic;
 using Cx.Compiler.Syntax;
+using Cx.Compiler.Syntax.Nodes;
+using CxParser = Cx.Compiler.Parser.Parser;
 
 namespace Cx.Compiler.Tests;
 
@@ -68,5 +72,118 @@ public sealed class ScopeResolverTests
             string.Join(Environment.NewLine, result.Diagnostics.Select(diagnostic => diagnostic.ToString())));
     }
 
+    [Fact]
+    public void Resolve_AttachesLocalSymbolToNameExpression()
+    {
+        var program = Parse(
+            """
+            fn main() -> int {
+                let value: int = 10;
+                return value;
+            }
+            """);
+        var model = Resolve(program);
+
+        var local = program.Functions.Single().Body.OfType<LetStatement>().Single();
+        var ret = program.Functions.Single().Body.OfType<ReturnStatement>().Single();
+        var name = Assert.IsType<NameExpressionNode>(ret.Expression);
+
+        Assert.Same(local.Semantic.Symbol, name.Semantic.Symbol);
+        Assert.Equal(SymbolKind.Local, name.Semantic.Symbol?.Kind);
+        Assert.True(model.RootScope.Children.Count > 0);
+    }
+
+    [Fact]
+    public void Resolve_AttachesParameterSymbolToNameExpression()
+    {
+        var program = Parse(
+            """
+            fn identity(value: int) -> int {
+                return value;
+            }
+            """);
+        Resolve(program);
+
+        var function = program.Functions.Single();
+        var parameter = function.Parameters.Single();
+        var ret = function.Body.OfType<ReturnStatement>().Single();
+        var name = Assert.IsType<NameExpressionNode>(ret.Expression);
+
+        Assert.Same(parameter.Semantic.Symbol, name.Semantic.Symbol);
+        Assert.Equal(SymbolKind.Parameter, name.Semantic.Symbol?.Kind);
+    }
+
+    [Fact]
+    public void Resolve_InnerLocalShadowsOuterLocal()
+    {
+        var program = Parse(
+            """
+            fn main() -> int {
+                let value: int = 1;
+                if (true) {
+                    let value: int = 2;
+                    return value;
+                }
+
+                return value;
+            }
+            """);
+        Resolve(program);
+
+        var function = program.Functions.Single();
+        var outer = function.Body.OfType<LetStatement>().Single();
+        var ifStatement = function.Body.OfType<IfStatement>().Single();
+        var inner = ifStatement.ThenBody.OfType<LetStatement>().Single();
+        var innerReturn = ifStatement.ThenBody.OfType<ReturnStatement>().Single();
+        var outerReturn = function.Body.OfType<ReturnStatement>().Single();
+        var innerName = Assert.IsType<NameExpressionNode>(innerReturn.Expression);
+        var outerName = Assert.IsType<NameExpressionNode>(outerReturn.Expression);
+
+        Assert.Same(inner.Semantic.Symbol, innerName.Semantic.Symbol);
+        Assert.Same(outer.Semantic.Symbol, outerName.Semantic.Symbol);
+    }
+
+    [Fact]
+    public void Resolve_AttachesGlobalSymbolWhenNoLocalShadowsIt()
+    {
+        var program = Parse(
+            """
+            let value: int = 10;
+
+            fn main() -> int {
+                return value;
+            }
+            """);
+        Resolve(program);
+
+        var global = program.GlobalVariables.Single();
+        var ret = program.Functions.Single().Body.OfType<ReturnStatement>().Single();
+        var name = Assert.IsType<NameExpressionNode>(ret.Expression);
+
+        Assert.Same(global.Semantic.Symbol, name.Semantic.Symbol);
+        Assert.Equal(SymbolKind.Global, name.Semantic.Symbol?.Kind);
+    }
+
     private static SourceFile Source(string path, string text) => new(path, text);
+
+    private static ProgramNode Parse(string source)
+    {
+        var diagnostics = new DiagnosticBag();
+        var program = new CxParser(diagnostics).Parse(new SourceFile("main.cx", source));
+        Assert.False(
+            diagnostics.HasErrors,
+            string.Join(Environment.NewLine, diagnostics.Diagnostics.Select(diagnostic => diagnostic.ToString())));
+        return program;
+    }
+
+    private static SemanticModel Resolve(ProgramNode program)
+    {
+        var diagnostics = new DiagnosticBag();
+        var model = new SemanticModel();
+        new ScopeResolver(diagnostics, model).Resolve(program);
+        Assert.False(
+            diagnostics.HasErrors,
+            string.Join(Environment.NewLine, diagnostics.Diagnostics.Select(diagnostic => diagnostic.ToString())));
+        return model;
+    }
 }
